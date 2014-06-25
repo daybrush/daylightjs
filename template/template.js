32fefc9807ae54b8b2fb248280b562e730ec97bb
Daybrush <itoolsg@gmail.com>
2014-06-25 13:44:42 +0900

    daylight all, all.in

---
+++ template/template.js
@@ -0,0 +1,58 @@
+/**
+* @func : daylight.template(object, template)
+* @param : object(Array, Object), template(String, Daylight)
+* @return : html(String)
+*/
+daylight.template = function(obj, template) {
+	var type = this.type(obj);
+	var templateType = this.type(template);
+	
+	if(templateType === sDaylight)
+		template = template.ohtml();//html 형태로 변환
+		
+	if(type === sArray) {//배열이면 리스트 형태로 만든다.
+		var contents = [];
+		var length = obj.length;
+		for(var i = 0; i < length; ++i) {
+			var content = obj[i];
+			contents[contents.length] = this.template(content, template);//배열의 요소를 다시 template을 만든다.
+		}
+		return contents.join(" ");
+	} else if(type === sObject) {//배열의 요소를 분석해서 {key}를 바꿔준다.
+		for(var k in obj) {
+			var value = obj[k];
+			if(this.type(value) === "array") {//만드는 중
+				var regx = new RegExp('{' + k + '}((.|\n|\r)*?){/'+ k + '}', 'g');
+				var list = template.match(regx);
+				
+				if(!list)
+					continue;
+				
+				for(var i = 0; i < list.length; ++i) {
+					var sub_template = list[i];
+					sub_template = sub_template.replace("{" + k + "}", "");
+					sub_template = sub_template.replace("{/" + k + "}", "");
+					template = template.replace(list[i], daylight.template(value, sub_template) );//{key} => value
+				}
+				
+			} else {
+				if(value === undefined)
+					value = "";
+				template = daylight.replace("{" + k + "}", value, template);//{key} => value
+			}
+		}
+		//console.log(template);
+		return template;
+	} else {
+		//배열이나 Dictionary 형태가 아닌 다른 것들은 키를 1로 하고 value로 바꿔준다.
+		return daylight.replace("{1}", obj, template);//{1} => value
+	}
+	return "";
+}
+
+
+
+prototype.template = function(o, t) {
+	this.html(daylight.template(o, t));
+	return this;
+}
